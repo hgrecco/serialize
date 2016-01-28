ce0e631a84f12705665eedf075477417f63bf0d2
Hernan Grecco <hernan.grecco@gmail.com>
2016-01-28 12:34:35 -0300

    Implemented MSGPACK format

---
+++ serialize/msgpack.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+    serialize.msgpack
+    ~~~~~~~~~~~~~~~~~
+
+    Helpers for Msgpack Serialization.
+
+    See https://pypi.python.org/pypi/msgpack-python for more details.
+
+    :copyright: (c) 2016 by Hernan E. Grecco.
+    :license: BSD, see LICENSE for more details.
+"""
+
+from . import all
+
+try:
+    import msgpack
+except ImportError:
+    all.register_unavailable('msgpack', pkg='msgpack-python')
+    raise
+
+
+def dumps(obj):
+    return msgpack.packb(obj, default=all.encode)
+
+
+def loads(content):
+    return msgpack.unpackb(content, object_hook=all.decode, encoding='utf-8')
+
+all.register_format('msgpack', dumps, loads)
