866457d56d19b0a62f6b239a9ccbbb2dc79bb76a
Hernan Grecco <hernan.grecco@gmail.com>
2016-01-28 12:35:14 -0300

    Implemented BSON format

---
+++ serialize/bson.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+    serialize.dill
+    ~~~~~~~~~~~~~~
+
+    Helpers for Bson Serialization.
+
+    See https://pypi.python.org/pypi/bson for more details.
+
+    :copyright: (c) 2016 by Hernan E. Grecco.
+    :license: BSD, see LICENSE for more details.
+"""
+
+from . import all
+
+try:
+    import bson
+except ImportError:
+    all.register_unavailable('bson', pkg='bson')
+    raise
+
+
+# In the BSON format the top level object must be a dictionary.
+# If necessary, we put the object in dummy dictionary
+# under the key __bson_follow__
+
+def dumps(obj):
+    if not isinstance(obj, dict):
+        obj = dict(__bson_follow__=obj)
+    return bson.dumps(all.traverse_and_encode(obj))
+
+
+def loads(content):
+    obj = all.traverse_and_decode(bson.loads(content))
+    return obj.get('__bson_follow__', obj)
+
+all.register_format('bson', dumps, loads)
