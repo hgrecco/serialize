17e99cc27e5bf52c5dbaa3b9517b310ae723fcff
Pieter Eendebak <pieter.eendebak@tno.nl>
2018-04-23 11:47:16 +0200

    add version information in module

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import re
 
 try:
     from setuptools import setup
@@ -11,6 +12,19 @@ except ImportError:
 
 import codecs
 
+def get_version():
+    """ Extract version information from source code """
+    try:
+        with open('serialize/version.py', 'r') as f:
+            ln = f.readline()
+            m = re.search('.* ''(.*)''', ln)
+            version = (m.group(1)).strip('\'')
+    except Exception as E:
+        print(E)
+        version = 'none'
+    return version
+
+version = get_version()
 
 def read(filename):
     return codecs.open(filename, encoding='utf-8').read()
@@ -24,7 +38,7 @@ __doc__ = long_description
 
 setup(
     name='Serialize',
-    version='0.2.dev0',
+    version=version,
     description='A common API for multiple serialization formats with support for custom classes',
     long_description=long_description,
     keywords='serialization deserialization packing unpacking ',
