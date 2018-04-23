b3879614c0c61e770aa71ecdd3c8fb827aa9d034
Pieter Eendebak <pieter.eendebak@tno.nl>
2018-04-23 11:47:56 +0200

    autopep

---
+++ serialize/simplejson.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+    serialize.dill
+    ~~~~~~~~~~~~~~
+
+    Helpers for JSON Serialization.
+
+    See https://docs.python.org/3/library/json.html for more details.
+
+    :copyright: (c) 2016 by Hernan E. Grecco.
+    :license: BSD, see LICENSE for more details.
+"""
+
+from . import all
+
+try:
+    import simplejson as json
+except ImportError:  # pragma: no cover
+    all.register_unavailable('json', pkg='json')
+    raise
+
+
+class Encoder(json.JSONEncoder):
+
+    def __init__(self, **kwargs):
+        kwargs['tuple_as_array'] = False
+        super().__init__(**kwargs)
+
+    def default(self, obj):
+        return all.encode(obj, super().default)
+
+
+def dumps(obj):
+    return json.dumps(obj, cls=Encoder, tuple_as_array=False).encode('utf-8')
+
+
+def dumps_pretty(obj):
+    return json.dumps(obj, cls=Encoder, sort_keys=True, tuple_as_array=False,
+                      indent=4, separators=(',', ': ')).encode('utf-8')
+
+
+def loads(content):
+    return json.loads(content.decode('utf-8'),
+                      object_hook=all.decode)
+
+
+# We create two different subformats for json.
+# The first (default) is compact, the second is pretty.
+
+all.register_format('simplejson', dumps, loads)
+all.register_format('simplejson:pretty', dumps_pretty, loads)
