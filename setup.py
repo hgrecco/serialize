e3dd6d6e9e9e66dbf9afa5db1f6d4deddeef2956
Hernan <hernan.grecco@gmail.com>
2021-12-23 13:42:04 -0300

    Migrated to from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,53 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+from setuptools import setup
 
-import sys
-
-try:
-    from setuptools import setup
-except ImportError:
-    print('Please install or upgrade setuptools or pip to continue')
-    sys.exit(1)
-
-import codecs
-
-
-def read(filename):
-    return codecs.open(filename, encoding='utf-8').read()
-
-
-long_description = '\n\n'.join([read('README.rst'),
-                                read('AUTHORS'),
-                                read('CHANGES')])
-
-__doc__ = long_description
-
-setup(
-    name='Serialize',
-    version='0.2.dev0',
-    description='A common API for multiple serialization formats with support for custom classes',
-    long_description=long_description,
-    keywords='serialization deserialization packing unpacking ',
-    author='Hernan E. Grecco',
-    author_email='hernan.grecco@gmail.com',
-    url='https://github.com/hgrecco/serialize',
-    test_suite='serialize.testsuite.testsuite',
-    zip_safe=True,
-    packages=['serialize'],
-    license='BSD',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ])
+if __name__ == "__main__":
+    setup()
