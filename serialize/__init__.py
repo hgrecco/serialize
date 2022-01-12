a9e107fd82f682c1fc9d6978a60f0b919d93f358
Hernan <hernan.grecco@gmail.com>
2022-01-12 14:52:00 -0300

    Added version reporting and use zest.releaser

--- serialize/__init__.py
+++ serialize/__init__.py
@@ -11,6 +11,20 @@
 
 from importlib import import_module
 
+try:
+    from importlib.metadata import version
+except ImportError:
+    # Backport for Python < 3.8
+    from importlib_metadata import version
+
+try:  # pragma: no cover
+    __version__ = version("serialize")
+except Exception:  # pragma: no cover
+    # we seem to have a local copy not installed without setuptools
+    # so the reported version will be unknown
+    __version__ = "unknown"
+
+
 # Modules that help serialize use other packages.
 
 _MODULES = (
